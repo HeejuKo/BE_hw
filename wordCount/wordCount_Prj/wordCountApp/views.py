9b905aba9409b8572481f996d2da887c4c8d2577
HeejuKo <khjg00gle96.gmail>
2024-03-29 17:30:05 +0900

    study1

--- wordCount/wordCount_Prj/wordCountApp/views.py
+++ wordCount/wordCount_Prj/wordCountApp/views.py
@@ -13,7 +13,6 @@ def result(request):
     word_list = entered_text.split() # entered_text를 공백 기준으로 문자열을 나눔
 
     word_dictionary = {}
-    word_count = 0
     noSpace_count = len(entered_text)
 
     for word in word_list:          # {word : 출현 횟수}로 만들어줌
@@ -21,13 +20,12 @@ def result(request):
             word_dictionary[word] += 1
         else:
             word_dictionary[word] = 1
-        word_count += 1
 
-    letter_count = noSpace_count - word_count + 1
+    letter_count = noSpace_count - len(word_list)
 
     # result.html 가져가서 사용할 변수 챙김
     return render(request, "result.html", {'alltext': entered_text, 'dictionary': word_dictionary.items(),
-                    'word': word_count, 'letter': letter_count, 'noSpace': noSpace_count})
+                    'word': len(word_list), 'letter': letter_count, 'noSpace': noSpace_count})
 
 def hello(request):
     name = request.GET['myname']
